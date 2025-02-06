53636ccffdffc1b9e4b88f86153ccdb8949fc8d5
tariq <rahee.programmer@gmail.com>
2025-02-06 09:52:47 +0500

    FormGroup And FormGroupName

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'reactive-form';
+export class AppComponent implements OnInit {
+  title = 'template-driven-form';
+
+  reactiveForm: FormGroup;
+
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl(null, Validators.required),
+      lastname: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      username: new FormControl(null, Validators.required),
+      dob: new FormControl(null),
+      gender: new FormControl(null),
+      address: new FormGroup({
+        street: new FormControl(null, Validators.required),
+        country: new FormControl(null, Validators.required),
+        city: new FormControl(null),
+        region: new FormControl(null),
+        postal: new FormControl(null, Validators.required),
+      }),
+    });
+  }
+
+  onFormSubmitted() {
+    console.log(this.reactiveForm);
+  }
 }
